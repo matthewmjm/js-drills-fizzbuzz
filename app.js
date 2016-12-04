d16819024a7e91db8bac6ab9f8a3012df9821555
matthewmjm <matthewmjm@yahoo.com>
2016-12-03 18:45:49 -0700

    completed exercise

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 function fizzBuzz(countTo) {
     // your code here
+    var result = [];
+    for (var i = 1; i <= countTo; i++) {
+        if (i % 15 === 0) {
+            result.push('fizzbuzz');
+        } else if (i % 5 === 0) {
+            result.push('buzz');
+        } else if (i % 3 === 0) {
+            result.push('fizz');
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
 }
 
 
